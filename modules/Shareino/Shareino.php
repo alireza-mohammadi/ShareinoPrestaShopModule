91884c567268ec6bf769de24111c16e313c266e6
Saeed Darvish <sd.saeed.darvish@gmail.com>
2016-10-25 12:32:24 +0330

    Define Shareino module to prestashop
    - Create Install and uninstall method
    - Create table on installation method
    - Drop Some Table on uninstallation metho
    - Register hooks

---
+++ modules/Shareino/Shareino.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * 2015-2016 Shareino
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is for module that make sync Product With shareino server
+ * https://github.com/SaeedDarvish/PrestaShopShareinoModule
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Shareino to newer
+ * versions in the future. If you wish to customize Shareino for your
+ * needs please refer to https://github.com/SaeedDarvish/PrestaShopShareinoModule for more information.
+ *
+ * @author    Saeed Darvish <sd.saeed.darvish@gmail.com>
+ * @copyright 2015-2016 Shareino Co
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  Tejarat Ejtemaie Eram
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Shareino extends Module
+{
+
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'Shareino';
+        $this->tab = 'export';
+        $this->version = '1.2.0';
+        $this->author = 'Saeed Darvish';
+        $this->need_instance = 1;
+        $this->module_key = '84e0bc5da856da1c414762d8fdfe9a71';
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Shareino');
+        $this->description = $this->l('Make Sync Your Product with shareino server');
+
+        $this->confirmUninstall = $this->l('if You unistall it you cant sync to shareino,Are you sure you want to uninstall?');
+
+        $token = Configuration::get('SHAREINO_API_TOKEN');
+
+        if ($token == "" || $token == null) {
+            $this->warning = $this->l('Shareino Token hasn\'t been set yet');
+        }
+    }
+
+    public function install()
+    {
+
+        if (Shop::isFeatureActive())
+            Shop::setContext(Shop::CONTEXT_ALL);
+
+        //Init
+        if (!ConfigurationCore::get("SHAREINO_API_TOKEN"))
+            ConfigurationCore::updateValue('SHAREINO_API_TOKEN', '');
+
+        include(dirname(__FILE__) . '/sql/install.php');
+
+        $this->installTabs();
+        
+        return parent::install() &&
+        $this->registerHook('header') &&
+        $this->registerHook('backOfficeHeader') &&
+        $this->registerHook('actionProductDelete') &&
+        $this->registerHook('actionProductSave') &&
+        $this->registerHook('actionUpdateQuantity');
+    }
+
+    public function uninstall()
+    {
+        include(dirname(__FILE__) . '/sql/uninstall.php');
+
+        return parent::uninstall();
+    }
+
+    public function hookActionProductDelete()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookActionProductSave()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookActionUpdateQuantity()
+    {
+        /* Place your code here. */
+    }
+
+    /**
+     * Add Shareino tabs to Prestashop main menu
+     */
+    public function installTabs()
+    {
+        // Install Tabs
+        $parent_tab = new Tab();
+        // Need a foreach for the language
+        $parent_tab->name[$this->context->language->id] = $this->l('Shareino');
+        $parent_tab->id_parent = 0; // Home tab
+        $parent_tab->class_name = 'AdminSynchronize';
+        $parent_tab->module = $this->name;
+        $parent_tab->add();
+
+
+        $tab = new Tab();
+        // Need a foreach for the language
+        $tab->name[$this->context->language->id] = $this->l('ّهمسان سازی محصولات');
+        $tab->class_name = 'AdminSynchronize';
+        $tab->id_parent = $parent_tab->id;
+        $tab->module = $this->name;
+        $tab->add();
+
+        $tab = new Tab();
+
+        // Need a foreach for the language
+        $tab->name[$this->context->language->id] = $this->l('معادل سازی دسته بندی ها');
+        $tab->class_name = 'AdminManageCats';
+        $tab->id_parent = $parent_tab->id;
+        $tab->module = $this->name;
+        $tab->add();
+    }
+}
---
+++ modules/Shareino/sql/install.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * 2015-2016 Shareino
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is for module that make sync Product With shareino server
+ * https://github.com/SaeedDarvish/PrestaShopShareinoModule
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Shareino to newer
+ * versions in the future. If you wish to customize Shareino for your
+ * needs please refer to https://github.com/SaeedDarvish/PrestaShopShareinoModule for more information.
+ *
+ *  @author    Saeed Darvish <sd.saeed.darvish@gmail.com>
+ *  @copyright 2015-2016 Shareino Co
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  Tejarat Ejtemaie Eram
+ */
+
+
+$sql = array();
+
+$sql[] = 'CREATE TABLE IF NOT EXISTS `PREFIX_shareino_sync` (
+  `id_shareino_sync` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NOT NULL,
+  `status` TINYINT NULL,
+  `errors` VARCHAR(500) NULL,
+  `date_add` DATETIME NULL,
+  `date_upd` DATETIME NULL,
+  PRIMARY KEY (`id_shareino_sync`),
+  UNIQUE INDEX `product_id_UNIQUE` (`product_id` ASC))DEFAULT CHARSET=utf8;';
+
+$sql[] = 'INSERT IGNORE INTO `PREFIX_shareino_sync` (`product_id`,`status`)
+            SELECT `id_product` AS `product_id`,0 AS `status`
+            from `PREFIX_product`;';
+
+$sql[] = 'CREATE TABLE IF NOT EXISTS `PREFIX_shareino_organized` (
+  `id_shareino_organized` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(50) DEFAULT NULL,
+  `cat_id` int(10) unsigned NOT NULL,
+  `ids` varchar(200) NOT NULL,
+  `names` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_shareino_organized`),
+  UNIQUE KEY `cat_id_UNIQUE` (`cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;';
+
+foreach ($sql as $query) {
+    if (Db::getInstance()->execute($query) == false) {
+        return false;
+    }
+}
